2ab29808cd34561007ac27abed1b80ce3e8d283c
Calo Corp <marc.calo@codeimmersives.com>
2020-02-11 10:11:44 -0500

    finished

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 function fizzy(num) {
+  if (num % 3 === 0 && num % 5 !== 0){
+  return 'Fizz'}
 
+if (num % 5 === 0 && num % 3 !== 0){
+  return 'Buzz'}
+
+if (num % 5 === 0 && num % 3 === 0) {
+
+  return 'FizzBuzz'}
+
+  if (num % 5 !== 0 && num % 3 !== 0) {
+
+    return num
+}  
 }
-
 module.exports = {
   fizzy,
 }
